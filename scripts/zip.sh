4e73c7b9e85eb700412215f5a17c3084eeadac08
tmdh <tarequemd.hanif@yahoo.com>
2019-07-25 14:06:36 +0300

    Update zip.sh

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php composer.phar create-project --prefer-dist --no-scripts laravel/laravel 5.8.29 foobar
+php composer.phar create-project --prefer-dist --no-scripts laravel/laravel=5.8.29 foobar
 mkdir app\\zip
 mkdir app\\zip\\vendors
 node ./app/js/zip.js
